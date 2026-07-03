d3038cab364f78b4cc7b22b460bf4ddd24cd16c7
SAMARTHKAUSHIK187 <samarthkaushik187@gmail.com>
2026-07-03 16:22:10 +0000

    Sync LeetCode submission - Task Scheduler (cpp)

---
+++ problems/task_scheduler/solution.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    int leastInterval(vector<char>& tasks, int n) {
+        int freq[26] = {0};
+        for(char& ch : tasks){
+            freq[ch-'A']++;
+        }
+
+        priority_queue<int> pq;
+        for(int i = 0; i < 26; i++){
+            if(freq[i] > 0){
+                pq.push(freq[i]);
+            }
+        }
+        int time = 0;
+        while(!pq.empty()){
+            int cycle = n+1;
+            vector<int>store;
+            int taskCnt = 0;
+            while(cycle-- && !pq.empty()){
+                if(pq.top() > 1){
+                    store.push_back(pq.top() - 1);
+                }   
+                pq.pop();
+                taskCnt++;
+            }
+            for(int &x : store){
+            pq.push(x);
+            }
+            time += (pq.empty())? taskCnt : n + 1;
+        }
+        return time;
+
+    }
+};
