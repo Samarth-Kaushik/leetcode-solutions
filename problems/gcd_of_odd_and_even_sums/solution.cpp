dc6f039c441319046eb642b41335d7469ad326d7
SAMARTHKAUSHIK187 <samarthkaushik187@gmail.com>
2026-07-15 05:16:06 +0000

    Sync LeetCode submission - GCD of Odd and Even Sums (cpp)

--- problems/gcd_of_odd_and_even_sums/solution.cpp
+++ problems/gcd_of_odd_and_even_sums/solution.cpp
@@ -1,11 +1,6 @@
 class Solution {
 public:
     int gcdOfOddEvenSums(int n) {
-        int evSum = 0, odSum = 0;
-       for(int i = 1; i <= 2*n; i++){
-            if(i & 1) odSum += i;
-            else evSum += i;
-       } 
-       return __gcd(evSum, odSum) ;
+       return __gcd(n*n + n, n*n + 2*n) ;
     }
 };
