693cbf31784a7ee870003179157dcc66e88326e3
SAMARTHKAUSHIK187 <samarthkaushik187@gmail.com>
2026-07-20 07:48:15 +0000

    Sync LeetCode submission - Shift 2D Grid (cpp)

---
+++ problems/shift_2d_grid/solution.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    vector<vector<int>> shiftGrid(vector<vector<int>>& grid, int k) {
+        int n = grid.size(), m = grid[0].size();
+
+        k = k % (n * m); 
+
+        while(k--) {
+            vector<vector<int>> temp(n, vector<int>(m));
+
+            for(int i = 0; i < n; i++){
+                for(int j = 0; j < m; j++){
+
+                    
+                    if(i == n-1 && j == m-1){
+                        temp[0][0] = grid[i][j];
+                    }
+                    
+                    else if(j == m-1){
+                        temp[i+1][0] = grid[i][j];
+                    }
+                    
+                    else{
+                        temp[i][j+1] = grid[i][j];
+                    }
+                }
+            }
+
+            grid = temp; 
+        }
+
+        return grid;
+    }
+};
