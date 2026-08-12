acd84dafd9880c9a23232884a44a394a49cd010e
SAMARTHKAUSHIK187 <samarthkaushik187@gmail.com>
2026-08-12 03:45:04 +0000

    Sync LeetCode submission - Length of Longest Subarray With at Most K Frequency (cpp)

---
+++ problems/length_of_longest_subarray_with_at_most_k_frequency/solution.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int maxSubarrayLength(vector<int>& nums, int k) {
+        int n = nums.size();
+        int i = 0, j = 0;
+        int ans = 0;
+        unordered_map<int, int> mpp;
+        for(j = 0; j < n; j++){
+            mpp[nums[j]]++;
+            while(mpp[nums[j]] > k){
+                mpp[nums[i]]--;
+                i++;
+            }
+            ans = max(ans, j-i+1);
+        }
+        return ans;
+    }
+};
