f7d93309b6bd44a362592406a48c2f40f2074607
SAMARTHKAUSHIK187 <samarthkaushik187@gmail.com>
2026-08-18 09:05:48 +0000

    Sync LeetCode submission - Isomorphic Strings (cpp)

--- problems/isomorphic_strings/solution.cpp
+++ problems/isomorphic_strings/solution.cpp
@@ -1,21 +1,18 @@
 class Solution {
 public:
     bool isIsomorphic(string s, string t) {
-        
-        int lastPosS[256] = {0};
-        int lastPosT[256] = {0};
-
-        for (int i = 0; i < s.size(); i++) {
-            
-            if (lastPosS[s[i]] != lastPosT[t[i]]) {
-                return false;
-            }
-
-            
-            lastPosS[s[i]] = i + 1;
-            lastPosT[t[i]] = i + 1;
+        if(s.size() != t.size()) return false;
+        unordered_map<char, int> mpp1;
+        unordered_map<char, int> mpp2;
+        for(int i = 0; i < s.size(); i++){
+            if(mpp1.find(s[i]) == mpp1.end()){
+                mpp1[s[i]] = i;
+            } 
+            if(mpp2.find(t[i]) == mpp2.end()){
+                mpp2[t[i]] = i;
+            } 
+            if(mpp1[s[i]] != mpp2[t[i]]) return false;
         }
-
         return true;
     }
 };
